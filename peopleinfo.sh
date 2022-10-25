cf0721821b541436b22d6523ec0fe5b1a3b100d8
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-10-25 17:40:36 +0100

    Now updated to display any parts of the example_people_data file that don't have 7 fields! also excludes headers by presence of the word _name_

--- peopleinfo.sh
+++ peopleinfo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cat example_people_data | awk '{
-
+grep -v "name" example_people_data.tsv | awk 'BEGIN{FS="\t" ;}
+    {
+        if(NF != 7)
+            { print "Not 7 fields: ", $0 ;}
+    }'
